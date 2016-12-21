5a961023ce78e2a8b5173ce6d000aa678932350f
Christian Knöpke <apfelbaum@email.de>
2016-12-21 21:20:25 +0100

    Update version.h

--- src/version.h
+++ src/version.h
@@ -30,13 +30,13 @@ static const int DATABASE_VERSION = 80600;
 // network protocol versioning
 //
 
-static const int PROTOCOL_VERSION = 82632; // Fork4 ++ before 82631
+static const int PROTOCOL_VERSION = 82633; // Fork4 ++ before 82631
 
 // intial proto version, to be increased after version/verack negotiation
 static const int INIT_PROTO_VERSION = 209;
 
 // disconnect from peers older than this proto version
-static const int MIN_PEER_PROTO_VERSION = 80060;
+static const int MIN_PEER_PROTO_VERSION = 82632;
 
 // nTime field added to CAddress, starting with this version;
 // if possible, avoid requesting addresses nodes older than this
